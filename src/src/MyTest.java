1f26e5b22f17ef0e6e17b8274072d2f00d750ddc
scottfly <he_zheng_lin@sina.com>
2016-08-14 20:34:47 +0800

    creat;

---
+++ src/src/MyTest.java
@@ -0,0 +1,19 @@
+package src;
+
+import graphql.GraphQL;
+import graphql.schema.GraphQLObjectType;
+import graphql.schema.GraphQLSchema;
+
+import static graphql.Scalars.GraphQLString;
+import static graphql.schema.GraphQLFieldDefinition.newFieldDefinition;
+import static graphql.schema.GraphQLObjectType.newObject;
+import java.util.Map;
+
+public class MyTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
